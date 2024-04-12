4100372657c213008ba12045a32e37935cda9ef0
Rudra Patel <83312632+Alphaknite@users.noreply.github.com>
2024-04-12 13:30:29 -0400

    init

---
+++ src/libs/mongodb.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI!);
+        console.log("MongoDB connected");
+    }
+    catch (err) {
+        console.error(err);
+    }  
+}
+
+export default connectMongoDB;
